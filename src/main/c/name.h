2425ef2cefa909102fa27bd69c12c0ba3d589744
vmurthy <venkateshamurthy@users.noreply.github.com>
2024-08-30 16:24:49 +0530

    Update name.h
    added comments

--- src/main/c/name.h
+++ src/main/c/name.h
@@ -1,13 +1,23 @@
 #include <stdarg.h>
 
-
-
+/* Structure to hold name & age. */
 typedef struct _name {
     char * first;
     char * last;
     int age;
 } name;
+
+/* Stringifier */
 char * name_to_string (name * n);
-void name_free(name * n);
+
+/* Free Memory */
+void   name_free(name * n);
+
+/* Create */
 name * name_create(const char * f, const char * l, int ag);
+
+/* 
+   A va (variable argument) list comprising different args.
+   This makes use of name_create after reading va_list.
+*/
 name * name_make( va_list ap);
