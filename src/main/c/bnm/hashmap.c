b2bc354a24f7a397a180e1bf77ff26f17317205e
vmurthy <venkateshamurthy@users.noreply.github.com>
2024-12-20 18:38:09 +0530

    Update hashmap.c - added store_number method

--- src/main/c/bnm/hashmap.c
+++ src/main/c/bnm/hashmap.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 #include <stdbool.h>
-#define SIZE 6
-
+#define SIZE 10
 
 int arr[SIZE] = {0};   // Hash table array
 int flag[SIZE]= {0};  // Flag array to mark filled slots
+void store_number (int num);
 
 int main() {
-
-    int num, loc, i, j;
+    printf("Hashmap program of SIZE:%d\n", SIZE);
+    printf("--------------------------\n");
+    int num, loc, i;
     // Input records
     for (i = 0; i < SIZE; i++) {
         //Enter data
-        printf("Enter record %d: ", i + 1);
+        printf("Enter record %d (of %d): ", i + 1, SIZE);
         scanf("%d", &num);
-
+        store_number(num);
+        /*
         // Calculate hash location
         loc = num % SIZE;
 
         // Check for collision
-        if ( ! flag[loc]) {
-            arr[loc] = num;
-            flag[loc] = true;
+        if ( ! flag[loc]) {            // No collision
+            arr[loc] = num;            // so store num in arr[loc]
+            flag[loc] = true;          // mark flag[loc] = true to indicate occupied
             printf("Stored at address %d\n", loc);
-        } else {
-            j    = loc + 1;            // start from next location
-            printf("Collision detected at address %d, trying next slot.\n", j);
+        } else {                       // Collision detected
+            printf("Collision detected at address %d, trying next slot from.\n", loc);
 
             // Resolve collision using linear probing
-            while (flag[j % SIZE])  // keep moving till you find a false
-                j++;
+            while (flag[++loc % SIZE])  // keep moving till you find a flag[loc%SIZE]=false
+                 ; //Do nothing
 
-            arr[j % SIZE] = num;    // store num at  j%SIZE
-            flag[j % SIZE] = true;  // store true at j%SIZE
-            printf("Stored at address %d\n", j % SIZE);
+            arr[loc % SIZE] = num;    // store num at  j%SIZE
+            flag[loc % SIZE] = true;  // store true at j%SIZE
+            printf("Stored at address %d\n", loc % SIZE);
         }
+        */
    }
 
 
@@ -46,6 +48,29 @@ int main() {
         } else {
             printf("Address %d: ##\n", i);
         }
+    }
+}
+
+// Store the number to has table
+void store_number (int num) {
+    // Calculate hash location
+    int loc = num % SIZE;
+
+    // Check for collision
+    if ( ! flag[loc]) {            // No collision
+        arr[loc] = num;            // so store num in arr[loc]
+        flag[loc] = true;          // mark flag[loc] = true to indicate occupied
+        printf("Stored at address %d\n", loc);
+    } else {                       // Collision detected
+        printf("Collision detected at address %d, trying next slot from.\n", loc);
+
+        // Resolve collision using linear probing
+        while (flag[++loc % SIZE])  // keep moving till you find a flag[loc%SIZE]=false
+        /** DO NOTHING */  ;
+
+        arr[loc % SIZE] = num;    // store num at  j%SIZE
+        flag[loc % SIZE] = true;  // store true at j%SIZE
+        printf("Stored at address %d\n", loc % SIZE);
     }
 }
 /*
